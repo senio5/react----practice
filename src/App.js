f90d7db2f9a7a8df506b34a00e812a8eac3d889b
senio5 <arsenio.taylor5@gmail.com>
2023-12-09 15:17:07 -0500

    react && practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(props){
+  //the "a" and "b" is like a short version of a database of messages which equals 2 Messages  
+  const[messages, setMessages] = React.useState(["a","b"])
+
+
+  return(
+    //conditional rendering using && operator
+    //first we get state number of messages which is 2
+    //second <h1> tells gives us a message saying we have 2 messages
+    //we place our javascript code or React code in curly braces with html tags <h1>
+    //which is called *-------------CONDITIONAL RENDERING------------*
+    <div>
+      {messages.length > 0 && <h1>You have {messages.length} unread messages</h1>}
     </div>
-  );
+  )
 }
-
-export default App;
